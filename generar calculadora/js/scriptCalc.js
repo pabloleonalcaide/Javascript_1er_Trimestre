1927ce559a923a4b7bee02b171abbd3da203caf8
Pablo <pabloleonpsico@gmail.com>
2017-11-25 12:53:55 +0100

    controlando operadores

--- generar calculadora/js/scriptCalc.js
+++ generar calculadora/js/scriptCalc.js
@@ -1,20 +1,20 @@
 /**
  * Script Calculadora como Objeto 
  * @author Pablo Leon
- * @version 1.0
+ * @version 1.1
  */
 {
     let calculadora = {
-        total : "",        
-        textBox: "",
+        textBox: "0",
         operando1: 0,
         operando2: 0,
+        hasComa: false,
         operador: "",
-        number : "0",
+        hasOperador: false,
         buttons : [
             "CE", "C", "%", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/", "0" ,"+/-", ",", "=" ],
         form : "",
-        result : "",
+        result : "0",
 
         /** Generate Form element */
         paintStructure: function() {
@@ -60,63 +60,99 @@
             for (let i = 0; i < calculadora.buttons.length; i++) {           
                 switch(calculadora.buttons[i]) {
                     case 'C':
-                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.clearLast;
-                    break;
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.clearLast;break;
                     case 'CE':
-                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.clearAll;
-                    break;
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.clearAll;break;
                     case ',':
-                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.setDecimal;
-                    break;
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.setDecimal;break;
+                    case '=': 
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.doOperation; break;
+                    case '%': 
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.percent; break;
+                    case '+/-':
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.changeSimbol; break;
                     default:
-                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.addNumber;
-                    break; 
+                    document.getElementById(calculadora.buttons[i]).onclick = calculadora.addNumber; break; 
                 }  
             }
         },
-        //Remodelar cuando añadamos funcionalidad de verdad
         addNumber : function() {
-            calculadora.total = calculadora.textBox.value;
-            if (calculadora.number === 0) {
-                if(this.value != 0 && this.value != '-'){    //no permitimos que ponga varios ceros seguidos al inicio
+            //Si había un 0 en pantalla
+            if (calculadora.textBox.value === "0"){
+                if(this.value ==='+' || this.value === '-' ||this.value ==='*' || this.value === '/' ){
+                }   
+                else if(this.value != 0){    //no permitimos que ponga varios ceros seguidos al inicio
                     calculadora.operando1 = this.value;
-                    calculadora.number = this.value;
-                }
-                calculadora.textBox.value = calculadora.number;
-                
-            }
-            //Posible mejora --> operandos en un array, y buscar con includes()
-            else if (calculadora.textBox.value ==='+' || calculadora.textBox.value === '-' ||calculadora.textBox.value ==='*' || calculadora.textBox.value === '/' ){
-                calculadora.textBox.value = this.value;
-                calculadora.operando2 = this.value;
-            }else {
-                if (this.value ==='+' || this.value === '-' ||this.value ==='*' || this.value === '/' ){
                     calculadora.textBox.value = this.value;
-                    calculadora.operador.value = this.value;
-                }                    
-                else 
-                    calculadora.textBox.value = (calculadora.total + this.value);                
+                }
+            //si lo que ha introducido es una operación                
+            }else if (this.value ==='+' || this.value === '-' ||this.value ==='*' || this.value === '/' ){
+                calculadora.operador=this.value;
+                calculadora.hasOperador = true;
+                
+            }else { //Si no hay una operación
+                if (calculadora.operador !=""){ //si hay un operador
+                    if (calculadora.operando2 === 0)
+                        calculadora.operando2 = this.value;
+                    else
+                        calculadora.operando2 += this.value;
+                    calculadora.textBox.value = calculadora.operando2
+                }                  
+                else{
+                    calculadora.operando1 += this.value;
+                    calculadora.textBox.value = calculadora.operando1;
+                }
+                    
+
             }
         },
         
-
+        //Despejar todo
         clearLast : function() {
             calculadora.textBox.value = calculadora.textBox.value.substring(0, calculadora.textBox.value.length-1)
             if(calculadora.textBox.value == "")
-                calculadora.textBox.value = 0;
+                calculadora.textBox.value = "0";
         },
         
         clearAll : function() {
-            calculadora.result = 0;
-            calculadora.number = 0;
-            calculadora.textBox.value = 0;
+            calculadora.textBox.value = "0";
         },
         
         setDecimal: function() {
             if(calculadora.textBox.value.indexOf(',') === -1) {
                 calculadora.textBox.value +=  this.value;                
-                
             }
+        },
+        changeSimbol: function(){
+                //
+        },
+        doOperation: function(){
+            switch(calculadora.operador){
+                case '+': 
+                    calculadora.textBox.value = (parseFloat(calculadora.operando1) + parseFloat(calculadora.operando2)); break;
+                case '-': 
+                    calculadora.textBox.value = (parseFloat(calculadora.operando1) - parseFloat(calculadora.operando2)); break;
+                case '*': 
+                    calculadora.textBox.value = (parseFloat(calculadora.operando1) * parseFloat(calculadora.operando2)); break;
+                case '/': 
+                    calculadora.textBox.value = (parseFloat(calculadora.operando1) / parseFloat(calculadora.operando2)); break;
+            }
+            calculadora.operando1 = calculadora.textBox.value;
+            calculadora.operando2 = 0;
+            calculadora.operador = "";
+        },
+
+        percent: function(){
+            if(calculadora.operando2 = 0){
+                        calculadora.textBox.value = (calculadora.operando1 /100);
+                        calculadora.operando1 = 0;
+            }
+        },
+        //comprobamos si es una operación
+        isOperation:function(){
+            if(this.value ==='+' || this.value === '-' ||this.value ==='*' || this.value === '/' )
+                return true;
+            return false;
         }
     };
  
