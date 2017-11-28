7e70cf70152efe58855879bfc7fdcdb81e9e3117
Pablo <pabloleonpsico@gmail.com>
2017-11-28 10:23:41 +0100

    modificado control iva

--- prototype en objeto factura/js/factura.js
+++ prototype en objeto factura/js/factura.js
@@ -2,6 +2,7 @@
  * @author Pablo Leon Alcaide
  * @version 1.0
  */
+
 /** Clase Cliente*/
 function Cliente(nombre, direccion, telefono, nif) {
   this.nombre = nombre;
@@ -14,15 +15,18 @@ function Elemento(descripcion, cantidad, precio) {
   this.descripcion = descripcion;
   this.cantidad = cantidad;
   this.precio = precio;
+  this.iva = "0.16";  //IVA es parte del estado del elemento
 }
+
 /** Clase factura, tiene un cliente y un conjunto de elementos*/
 function Factura(cliente, elementos) {
+  this.id = 0;
   this.cliente = cliente;
   this.elementos = elementos;
   this.informacion = {
     baseImponible:  0,
-    iva:            16,
     total:          0,
+    totalIva:       0, 
     formaPago:      "contado"
   };
 };
@@ -40,16 +44,24 @@ Factura.prototype.calculaTotal = function() {
   for(let i=0; i<this.elementos.length; i++) {
     this.informacion.baseImponible += this.elementos[i].cantidad * this.elementos[i].precio;
   }
-  this.informacion.total = this.informacion.baseImponible * this.informacion.iva;
+  this.informacion.total = this.informacion.baseImponible;
 }
-
+/**  --------------------------------------------------------------->>> Esto hay que corregirlo no repetir código */
+//Quizás con un flag o que el mismo recorrido modifique this.informacion.total y this.informacion.total.iva
+Factura.prototype.calculaTotalIva = function(){
+ for(let i=0; i<this.elementos.length; i++) {
+    this.informacion.totalIva += (this.elementos[i].cantidad * this.elementos[i].precio * this.elementos[i].iva)+(this.elementos[i].cantidad * this.elementos[i].precio);
+  }
+}
 /** Añadimos al prototipo de la empresa la función muestraTotal */
 Factura.prototype.muestraTotal = function() {
   this.calculaTotal();
-  document.write("Total = " + this.informacion.total + " Euros");
+  this.calculaTotalIva();
+  document.write("Total = " + this.informacion.total + " Euros<br>");
+  document.write("Total con IVA = " + this.informacion.totalIva + " Euros");
 }
 
-
+/** Inicialización de los objetos*/
 let cliente = new Cliente("Pablo", "bajo el mar", "699123456", "123456789P");
 let elementos = [new Elemento("elemento1", "1", "8"),
                   new Elemento("elemento2", "2", "34"),
