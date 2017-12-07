5d0a118d3f68544a8c340b15c24e86e16acf5d8d
Pablo <pabloleonpsico@gmail.com>
2017-12-07 13:29:05 +0100

    añadidas excepciones

--- Lindo_Gatito/js/functions.js
+++ Lindo_Gatito/js/functions.js
@@ -8,34 +8,28 @@
  */
  let invoque = function(){
  	let catWindow = window.open('','','width=600,height=450');
+ 	try{
  	catWindow.location.href = './catMenu.html';
  	loadSetting(catWindow);
+ 	}catch(e){
+ 		catWindow.close();
+ 		alert(e.message);
+ 	}
  }
 
-/**
- * Invoca una horda de gatos
- */
-let invoqueHorde = function(){
-	for (let i = 10; i >= 0; i--) {
-		invoque();
-	}
-}
 
 /**
  * Carga las variables del gato en la nueva ventana
  */
-let loadSetting = function (window1){
- 		let name = document.getElementById("name").value;
-	 	let race = document.getElementById("race").value;
-	 	let year = document.getElementById("year").value;
-	 	let weight = document.getElementById("weight").value;
- 		
- 		let cat = new Kitty(name,year,race,weight);
-		window1.newCat = cat;
+let loadSetting = function (window1){  
+  			let name = document.getElementById("name").value;
+		 	let race = document.getElementById("race").value;
+		 	let year = document.getElementById("year").value;
+		 	let weight = document.getElementById("weight").value;
+	 		let cat = new Kitty(name,year,race,weight);
+			window1.newCat = cat;
 
  	}
 
 document.getElementById("submit").addEventListener("click", invoque);
 
-/* Descomentar para habilitar función especial 'llamar a horda de gatos' */
-//document.getElementById("horda").addEventListener("click", invoqueHorde);
--- Lindo_Gatito/js/gatito.js
+++ Lindo_Gatito/js/gatito.js
@@ -1,5 +1,3 @@
-//INTRODUCIR EXCEPCIONES (SI INTENTA CREAR UN GATO CON PESO POR ENCIMA O POR DEBAJO  DE LO PERMITIDO)
-
 // CADA CAJA DE TEXTO TIENE SU PROPIO MENSAJE DE ERROR (AL PERDER EL FOCO);
 /**
  * Clase Gato para ejercicio 'Lindo Gatito'
@@ -8,19 +6,18 @@
 {
 	function Kitty(name,year,race,weight){
 	this.setName(name);
-	this.setWeight(weight);
-	this.year = year,
+	this.weight= Kitty.prototype.checkWeight(weight);
+	this.setYear(year),
 	this.race = race,
-	this.weight = weight,
 	this.isAlive = true,
 	this.state = 'sleeping';
+	this.id = Kitty.prototype.id++;
 	}
-	
+	Kitty.prototype.id = 1;	
 	Kitty.prototype.MAXPESO = 15;
 	Kitty.prototype.MINPESO = 1;
 
 	Kitty.prototype.setName = function(newName){
-		
 		this.name = newName;
 	}
 	Kitty.prototype.getName = function(){
@@ -35,11 +32,20 @@
 	Kitty.prototype.getWeight = function(){
 		return this.weight;
 	}
-
+	Kitty.prototype.checkWeight = function(weight){
+		if(weight>=Kitty.prototype.MAXPESO || weight <= Kitty.prototype.MINPESO)
+			throw new Error('El peso es inválido');
+		return weight;
+	}
 	Kitty.prototype.setWeight = function(mod){
-		if(this.weight>=this.MAXPESO || this.weight <= this.MINPESO)
-			throw 'El peso es inválido';
+		if(this.weight>=Kitty.prototype.MAXPESO || this.weight <= Kitty.prototype.MINPESO)
+			throw new Error('El peso es inválido');
 		this.weight = parseInt(this.weight) + parseInt(mod);
+	}
+	Kitty.prototype.setYear = function(year){
+		if(year < 2000)
+			throw new Error ('El año es inválido')
+		return year;
 	}
 	/** Comprueba si el gato está vivo*/
 	Kitty.prototype.checkLive = function(){
