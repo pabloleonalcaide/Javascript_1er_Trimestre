bd08a987fe673a2379076b11be2c93081af388e3
Pablo <pabloleonpsico@gmail.com>
2017-12-12 10:36:34 +0100

    eliminando duplicados

--- Ejercicios LIbrosWeb 1-7/js/codigo.js
+++
@@ -1,7 +0,0 @@
-//Pablo Leon Alcaide
-//Guardamos el mensaje en una variable global
-var mensaje1 = 'Hola Mundo!\n Qué fácil es incluir \'comillas simples\'\ny\"comillas dobles\"';
-//Primer mensaje que aparece. alert() despliega una ventana emergente
-alert(mensaje1);
-//Segundo mensaje que aparece
-alert("Soy el primer script");
--- Ejercicios LIbrosWeb 1-7/js/scriptEjercicio7.js
+++
@@ -1,6 +0,0 @@
-var number = prompt("Introduce un número, calcularé por tí su factorial");
-var result = 1;
-for (var i = 1; i <= number; i++) {
-    result = result * i;
-}
-alert("El resultado es: " + result + " Gracias por confiar en JS");
