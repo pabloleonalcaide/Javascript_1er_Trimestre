057e6a2721f6ecbeaab146a1ae058234740637b1
Pablo <pabloleonpsico@gmail.com>
2017-12-12 10:34:56 +0100

    Eliminando duplicados

--- Ejercicios 1-7 Pablo León Alcaide/js/scriptEjercicio3.js
+++
@@ -1,5 +0,0 @@
-//Pablo León ALcaide
-//Array que almacena los 12 meses del año
-var meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-
-alert(meses);
